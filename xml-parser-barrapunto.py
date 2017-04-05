edb09b384e32a90489a99996fef054065012d7ba
Your Name <you@example.com>
2017-04-05 12:20:47 +0200

    cambios menores

--- xml-parser-barrapunto.py
+++ xml-parser-barrapunto.py
@@ -72,8 +72,10 @@ rssFile.close()
 htmlFile = open("barrapunto.html", "w")
 htmlFile.write("<head><meta http-equiv='Content-Type' content='text/html;" +
                "charset=utf-8'/></head><br>\n")
+
 xmlFile = open("barrapunto.rss", "r")
 theParser.parse(xmlFile)
+
 htmlFile.close()
 
 print("Parse complete")
